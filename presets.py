dfaf3f48c12c7004108d7bb0a4af97d96c2ae12b
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-06-11 19:56:26 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -4,14 +4,14 @@ class Presets(object):
     CAPTION_TEXT_DOC = "\n\n<b>File Name:</b> {}\n\n<b>Format:</b> {}\n<b>Size:</b> {}"
     CAPTION_TEXT_VID = "\n\n<b>File Name:</b> {}\n\n<b>Size:</b> {}"
     ASK_PM_TEXT = "<b>Click the below button</b>"
-    WELCOME_TEXT = "<b>Hi <b>{}</b>\n\n<code>I can help you getting movies from</code> @Kidozkii. " \
-                   "<code>Just Keep this message live Here</code>😉\n\n" \
+    WELCOME_TEXT = "<b>Hi <b>{}</b>\n\n<code>I can help you to get movies in</code> [Kidozkii](https://t.me/Kidozkii). " \
+                   "<code>Btw, Just Keep this message live Here</code>😉...\n\n" \
                    "<b>I’m created & maintained by </b><a href='https://t.me/MarineBots'> Marine Bots</a>.</b>"
     CLEAN_CHAT_MSG = "⚠️ <b>Deleting all messages..</b>"
     MSG_FOR_PIN = "<b>For getting medias from here..</b>\n\n🔛 <code>Please start</code> @{} <code>in PM\n\n" \
-                  "Send the exact Movie name.\n\n🔊 I'll reply the file in PM if available in our channel !</code>"
+                  "Send the exact Movie name.\n\n🔊 I'll reply the file in PM if available in our channel!</code>"
 
-    BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
+    BOT_PM_TEXT = "<b>Sorry.. 😢</b>\n\n<code>Bot won't work in PM, Ask in ma Group. I'll reply the file in PM if " \
                   "available in our DB!</b>"
     PM_ERROR = "<b>Unable to send medias</b> ⛔️\n<code>Click the below button\nAsk here for movies later!</code>"
     MEDIA_SEND_TEXT = "<code>Media dispatched as PM 🥳</code>"
